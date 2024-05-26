18b3faffb60bd3752a6b27ffd1021d8386050ff9
Juan Arroyo <74197660+JuaArr@users.noreply.github.com>
2024-05-26 00:33:01 -0500

    mesh v2.0

--- makefile
+++ makefile
@@ -4,4 +4,5 @@ py = python3
 all: generate_mesh
 
 generate_mesh: $(gmsh_mesh_dir)/gmshMesh.py
+#	$(py) $^ 1> $(gmsh_mesh_dir)/gmshSummary.txt 2> $(gmsh_mesh_dir)/gmshEW.txt
 	$(py) $^
--- mesh_generation/gmshMesh.py
+++ mesh_generation/gmshMesh.py
@@ -1,63 +1,109 @@
 import gmsh
 
-# Using depth to simulate a 3D domain according to OpenFOAM specifications
-back = 0
-front = 0.1
+# --------------------------- Constants ---------------------------
 
-# Circle properties according to (x-cx)^2 + (y-cy)^2 = r^2  
-cx = 0.25
-cy = 0.1
-r = 0.05
+# Circle properties: (x-cx)^2 + (y-cy)^2 = r^2  
+cx = 25
+cy = 10
+r = 5
 
-# Rectangle properties according to (minX, maxX) X (minY, max Y)
+# Parallelepid dimensions: (minX, maxX) X (minY, maxY) X (minZ, maxZ)
 minX = 0
-maxX = 1
+maxX = 100
 minY = 0
-maxY = 0.2
+maxY = 20
+minZ = 0
+maxZ = 10
 
-# Meshsize
-ms1 = 0.01
-ms2 = 0.005
+# Standard mesh size
+lc = 1
+
+# --------------------------- Initial Set Up ---------------------------
 
 gmsh.initialize()
 gmsh.clear()
 
-gmsh.model.add("von Karman vortex mesh")
+# Set aliases
+option = gmsh.option
+model = gmsh.model
+occ = model.occ
+mesh = model.mesh
 
-# Set an alias to the geometry
-geometry = gmsh.model.occ
+# Add model name
+model.add("von Karman vortex mesh")
+
+# --------------------------- Core Geometry ---------------------------
 
 # Creation of channel
-geometry.addBox(minX, minY, back, maxX, maxY, front, 1)
+occ.addBox(minX, minY, minZ, maxX, maxY, maxZ, 1)
 
 # Creation of obstacle
-geometry.addCylinder(cx, cy, back, 0, 0, front, r, 2)
+occ.addCylinder(cx, cy, minZ, 0, 0, maxZ, r, 2)
 
 # Adding the hole
-geometry.cut([(3, 1)], [(3, 2)], 3)
+occ.cut([(3, 1)], [(3, 2)], 3)
 
 # Synchronizing cut
-geometry.synchronize()
+occ.synchronize()
+
+# --------------------------- Physical Groups ---------------------------
 
 # Set Physical Groups
-gmsh.model.addPhysicalGroup(2, [1], 2001, name = "Inlet")
-gmsh.model.addPhysicalGroup(2, [6], 2002, name = "Outlet")
-gmsh.model.addPhysicalGroup(2, [2, 4], 2003, name = "Wall")
-gmsh.model.addPhysicalGroup(2, [7], 2004, name = "Hole")
-gmsh.model.addPhysicalGroup(2, [3, 5], 2005, name = "BackAndFront")
-gmsh.model.addPhysicalGroup(3, [3], 3001, name = "Fluid")
+model.addPhysicalGroup(2, [1], 2001, name = "Inlet")
+model.addPhysicalGroup(2, [6], 2002, name = "Outlet")
+model.addPhysicalGroup(2, [2, 4], 2003, name = "Wall")
+model.addPhysicalGroup(2, [7], 2004, name = "Hole")
+model.addPhysicalGroup(2, [3, 5], 2005, name = "BackAndFront")
+model.addPhysicalGroup(3, [3], 3001, name = "Fluid")
 
 # Synchronizing physical groups
-geometry.synchronize()
+occ.synchronize()
 
-gmsh.model.mesh.generate(dim = 3)
-gmsh.model.mesh.refine()
+# --------------------------- Boundary Layer ---------------------------
 
-# surface_tags = gmsh.model.getEntities(dim=2)
-# for tag in surface_tags:
-#     if tag[1] == 1:  # Refine surface with tag 1 (for example)
-#         gmsh.model.mesh.refine([(tag[0], tag[1])], algorithm=0)
+# Set up of the mesh size according to the distance to the "Wall" and "Hole"
+mesh.field.add("Distance", 1)
+mesh.field.setNumbers(1, "SurfacesList", [2, 4, 7])
+mesh.field.setNumber(1, "Sampling", 100)
+
+# Set up of the mesh size according to a threshold
+mesh.field.add("Threshold", 2)
+mesh.field.setNumber(2, "InField", 1)
+mesh.field.setNumber(2, "SizeMin", lc/10)
+mesh.field.setNumber(2, "SizeMax", lc)
+mesh.field.setNumber(2, "DistMin", 0)
+mesh.field.setNumber(2, "DistMax", 3)
+
+# Using the threshold mesh size field
+mesh.field.setAsBackgroundMesh(2)
+
+# Using a good practice to avoid over-refinement
+option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
+option.setNumber("Mesh.MeshSizeFromPoints", 0)
+option.setNumber("Mesh.MeshSizeFromCurvature", 0)
+
+# --------------------------- Structured Mesh ---------------------------
+
+# for curve in occ.getEntities(1):
+#     mesh.setTransfiniteCurve(curve[1], 15)
+
+# for surf in occ.getEntities(2):
+#     mesh.setTransfiniteSurface(surf[1])
+
+# option.setNumber('Mesh.QuasiTransfinite', 1)
+
+# --------------------------- Mesh Generation ---------------------------
+
+# Mesh algorithm
+# option.setNumber('Mesh.RecombineAll', 1)
+# option.setNumber('Mesh.RecombinationAlgorithm', 1)
+# option.setNumber('Mesh.Recombine3DLevel', 2)
+option.setNumber('Mesh.ElementOrder', 1)
+
+# Mesh generation
+mesh.generate(dim = 3)
+
+# --------------------------- Save and Close ---------------------------
 
 gmsh.write("mesh_generation/gmshMesh.msh")
-
 gmsh.finalize()
