d6346b1c220f8f9e0ff2fef0c298fde863256e73
Juan Arroyo <74197660+JuaArr@users.noreply.github.com>
2024-05-30 16:26:21 -0500

    parallel_sh

---
+++ parallel_simulation/runAll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /usr/lib/openfoam/openfoam2312/etc/bashrc
+
+cd parallel_simulation
---
+++ parallel_simulation/runPre.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /usr/lib/openfoam/openfoam2312/etc/bashrc
+
+cd parallel_simulation
+
+blockMesh &> /dev/null
+
+read nproc
+
+sed "s/nOS/$nproc/" system/decomposeParDict-draft > system/decomposeParDict
+
+decomposePar &> /dev/null
