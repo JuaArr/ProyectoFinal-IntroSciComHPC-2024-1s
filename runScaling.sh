688b61339325d95b2a82747abbb9c96d4b40fb3e
Juan Arroyo <74197660+JuaArr@users.noreply.github.com>
2024-05-31 23:55:20 -0500

    scaling

--- runScaling.sh
+++ runScaling.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+echo "Running for 1 thread"
 bash serial_simulation/runPre.sh
 bash serial_simulation/runAll.sh
 bash serial_simulation/runClean.sh
 
 for nth in {2..4}
 do
+	echo "Running for $nth thread"
 	bash parallel_simulation/runPre.sh $nth
 	bash parallel_simulation/runAll.sh
-	bash parallel_simulation/runAll.sh
+	bash parallel_simulation/runClean.sh
 done
 
 bash scaling/runPlot.sh
