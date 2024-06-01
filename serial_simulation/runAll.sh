f9bc3f261a23a5e5577c2ab2a216b88cdabc1724
Juan Arroyo <74197660+JuaArr@users.noreply.github.com>
2024-05-31 21:31:07 -0500

    formatting wall time

--- serial_simulation/runAll.sh
+++ serial_simulation/runAll.sh
@@ -4,4 +4,4 @@ source /usr/lib/openfoam/openfoam2312/etc/bashrc
 
 cd serial_simulation
 
-{ time pimpleFoam > log.FOAMout 2> log.FOAMerror; } 2> time_1.log
+{ time pimpleFoam > log.FOAMout 2> log.FOAMerror; } 2>&1 | grep real | awk '{print $2}' | awk -Fm '{print $1*60 + $2}' > time_1.log
