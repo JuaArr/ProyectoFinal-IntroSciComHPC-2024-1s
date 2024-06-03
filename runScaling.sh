8968ed236b9c557ecf85e2799f48e7e0793209aa
juaarr <julopezar@unal.edu.co>
2024-06-03 00:55:44 -0500

    refinamiento de malla

--- runScaling.sh
+++ runScaling.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "Running for 1 thread"
-bash serial_simulation/runPre.sh
-bash serial_simulation/runAll.sh
-bash serial_simulation/runClean.sh
+run_simulation () {
+	if [ $1 -eq 1 ]
+	then
+		bash serial_simulation/runPre.sh
+		bash serial_simulation/runAll.sh
+		bash serial_simulation/runClean.sh
+	else
+		bash parallel_simulation/runPre.sh $1
+		bash parallel_simulation/runAll.sh
+		bash parallel_simulation/runClean.sh
+	fi
+}
 
-for nth in {2..4}
+for nth in {1..16}
 do
-	echo "Running for $nth thread"
-	bash parallel_simulation/runPre.sh $nth
-	bash parallel_simulation/runAll.sh
-	bash parallel_simulation/runClean.sh
+	echo "Running for $nth threads ..."
+	run_simulation $nth
 done
 
+echo "Plotting ..."
 bash scaling/runPlot.sh
